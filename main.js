91388097f11e22324fbc369cc19fe58a78d47447
Pedro Colis <colis321@yahoo.com>
2020-07-14 19:28:25 -0700

    working on dekstop modal popup

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 const shareTabOpen = document.querySelector('.information');
 const shareTabClosed = document.querySelector('.share-items');
 
+const desktopShare = document.querySelector('.desktop-share');
+const shareIcon = document.querySelector('.share');
+const closeIcon = document.querySelector('.share-closed');
+
+
 
 
 shareTabOpen.addEventListener('click', function () {
-  shareTabClosed.style.display = 'flex';
-  shareTabOpen.style.display = 'none';
+  if (window.innerWidth < 768) {
+    shareTabOpen.style.display = 'none';
+    shareTabClosed.style.display = 'flex';
+  }
 });
 
 shareTabClosed.addEventListener('click', function () {
-  shareTabClosed.style.display = 'none';
-  shareTabOpen.style.display = 'flex';
+  if (window.innerWidth < 768) {
+    shareTabClosed.style.display = 'none';
+    shareTabOpen.style.display = 'flex';
+  }
+});
+
+
+
+shareIcon.addEventListener('click', function (e) {
+  //esktopShare.style.display = 'flex';
+  if (e.target.classList.contains('share-icon')) {
+    console.log('anchor')
+  }
+
+
 });
